f820d093744ba9e5a7f429dff1ba97b92a042939
rizwann <rizwankabirsizan@gmail.com>
2021-01-04 22:01:29 +0100

    4/1/2021

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart =props.cart;
+    console.log(cart);
+    //const total = cart.reduce((total, prd)=> total+ prd.price,0)
+   let total = 0;
+   for(let i=0; i<cart.length; i++){
+       const product =cart[i];
+       total = total+ product.price;
+   }
+   let shipping = 0;
+   if (total>50){
+       shipping =0;
+   }
+   else if(total>30){
+       shipping= 4.99;
+   }
+   else if(total>0){
+       shipping= 12.99;
+   }
+   const tax = (total/10);
+
+const grandTotal= total+shipping+tax; 
+const formatNumber = num => {
+    const precision = num.toFixed(2);
+    return Number(precision);
+}
+
+    return (
+        <div>
+            <h4>Order Summary</h4>
+            <p>Items Added: {cart.length}  </p>
+            <p>Prodcut Price: {formatNumber(total)}</p>
+            <p><small>Shipping cost: {formatNumber(shipping)}</small></p>
+            <p><small>Tax + VAT: {formatNumber(tax)}</small></p>
+            <p>Total Price: {formatNumber(grandTotal)}</p>
+        </div>
+    );
+};
+
+export default Cart;
